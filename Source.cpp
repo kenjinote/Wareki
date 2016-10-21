1229da396c0997e62df1be55195469afb1103770
kenji <kenji@DESKTOP-LTA0N0K>
2016-10-22 01:58:14 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hList;
+	static HWND hButton;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hList = CreateWindow(TEXT("LISTBOX"), 0, WS_VISIBLE | WS_CHILD | WS_VSCROLL | LBS_NOINTEGRALHEIGHT, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("和暦取得"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		PostMessage(hWnd, WM_COMMAND, IDOK, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hList, 0, 0, 256, 512, TRUE);
+		MoveWindow(hButton, 0, 512, 256, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			SendMessage(hList, LB_RESETCONTENT, 0, 0);
+			TCHAR szText1[1024];
+			TCHAR szText2[1024];
+			SYSTEMTIME tDate;
+			GetSystemTime(&tDate);
+			const int nMax = tDate.wYear;
+			for (int i = 1873; i <= nMax; ++i)
+			{
+				tDate.wYear = i;
+				if (!GetDateFormat(NULL, DATE_USE_ALT_CALENDAR, &tDate, TEXT("ggyy年"), szText1, 1024))continue;
+				if (!GetDateFormat(DATE_LONGDATE, 0, &tDate, TEXT("西暦yyyy年"), szText2, 1024))continue;
+				lstrcat(szText1, szText2);
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)szText1);
+			}
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
